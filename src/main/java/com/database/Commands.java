c276d15b8b7fcfda28d2adb20ed5eaab9604b4e2
Silas Jul Kierstein <silaskierstein@gmail.com>
2024-03-07 11:57:22 +0100

    First push

---
+++ src/main/java/com/database/Commands.java
@@ -0,0 +1,14 @@
+package com.database;
+
+import java.sql.SQLException;
+
+public interface Commands {
+
+    public void Connect() throws SQLException;
+    public void insertImage(Image image);
+    public Image getImage(int imageID) throws SQLException;
+    public Image getImage(String imageID) throws SQLException;
+    public void sql(String query) throws SQLException;
+
+
+}
---
+++ src/main/java/com/database/DatabaseCommands.java
@@ -0,0 +1,53 @@
+package com.database;
+
+import org.postgresql.util.PSQLException;
+
+import java.sql.*;
+
+public class DatabaseCommands implements Commands {
+    private Statement st;
+    @Override
+    public void Connect() throws SQLException {
+        String url = "jdbc:postgresql://localhost:5432/postgres";
+        String user = "postgres";
+        String pass = "0000";
+
+        Connection con = DriverManager.getConnection(url, user, pass);
+        st = con.createStatement();
+    }
+
+    @Override
+    public void insertImage(Image image) {
+
+    }
+
+    @Override
+    public Image getImage(String imageID) throws SQLException {
+        ResultSet rs = st.executeQuery(String.format("SELECT base64 FROM images WHERE id = %s", imageID));
+        try {
+            rs.next();
+            System.out.println(rs.getString(1));
+            return null;
+        } catch (PSQLException e) {
+            System.err.println("Image does not exist");
+            return null;
+        }
+    }
+
+    @Override
+    public Image getImage(int imageID) throws SQLException {
+        return getImage(""+imageID);
+    }
+
+    @Override
+    public void sql(String query) throws SQLException {
+        st.executeQuery(query);
+    }
+
+
+    public static void main(String[] args) throws SQLException {
+        DatabaseCommands db = new DatabaseCommands();
+        db.Connect();
+        db.getImage(1);
+    }
+}
