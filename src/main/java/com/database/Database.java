44df9f92c459578655a165af5e93d8a3bceec9d7
Silas Jul Kierstein <silaskierstein@gmail.com>
2024-03-12 11:31:22 +0100

    simplificeret

--- src/main/java/com/database/Database.java
+++ src/main/java/com/database/Database.java
@@ -3,25 +3,19 @@ package com.database;
 import org.postgresql.util.PSQLException;
 import io.github.cdimascio.dotenv.Dotenv;
 import java.sql.*;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
-import java.sql.SQLException;
 
 public class Database implements Commands {
     private Statement st;
-private final Dotenv dotenv = Dotenv.load(); // load .env file
-Connection connection ; // connection to the database
+    private final Dotenv dotenv = Dotenv.load();
 
 
 
     Database() throws SQLException {
         // Drop table and create new
         Connect();
-        st = connection.createStatement();
         st.execute("DROP TABLE IF EXISTS images");
         st.execute("""
-                CREATE TABLE IF NOT EXISTS images (
+                CREATE TABLE images (
                 id SERIAL PRIMARY KEY,
                 base64 varchar not null,
                 tag varchar(69) not null
@@ -35,23 +29,15 @@ Connection connection ; // connection to the database
                 ('image4', 'animal');
                 """);
     }
-    public boolean isConnected() {
-        return connection != null;
-    }
+
     @Override
-    public void Connect()  {
-        if(connection == null) {
-            try {
-                connection = DriverManager.getConnection("jdbc:postgresql://" + dotenv.get("db_host") + ":" + dotenv.get("db_port") + "/" + dotenv.get("db_name"), dotenv.get("db_user"), dotenv.get("db_password"));
-                System.out.println("Connected to postgres database");
-            }catch (SQLException e) {
-                System.out.println("Failed to connected to postgres database + " + e.getCause() + e.getMessage());
+    public void Connect() throws SQLException {
+        String url = "jdbc:postgresql://localhost:5432/postgres";
+        String user = dotenv.get("db_user");
+        String pass = dotenv.get("db_password");
 
-            }
-
-
-        }
-
+        Connection con = DriverManager.getConnection(url, user, pass);
+        st = con.createStatement();
     }
 
     @Override
