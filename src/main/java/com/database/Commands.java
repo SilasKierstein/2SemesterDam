2dd1464f8023116703fdd50d6c2d1c2926d69986
Lars Kristiansen <larkr23@student.sdu.dk>
2024-04-03 11:44:44 +0200

    add some notes to interface

--- src/main/java/com/database/Commands.java
+++ src/main/java/com/database/Commands.java
@@ -4,9 +4,19 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public interface Commands {
-
+    /**
+     *
+     * @throws SQLException
+     */
     public void Connect() throws SQLException;
-    public void insertImage(Image image);
+
+    /**
+     * This method is used to upload a new image
+     * @param image to be uploaded
+     * @return ID of the saved image
+     */
+
+    public int insertImage(Image image); // TODO return id
     public Image getImage(int imageID) throws SQLException;
     public Image getImage(String imageID) throws SQLException;
 
